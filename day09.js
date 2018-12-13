d080360c1ffaa91eba97d7cacebb6e62141cbb06
Cheng Leong <leongc@alumni.rice.edu>
2018-12-13 02:37:47 -0600

    Update day09.js
    add trailing ) to part 2 which is now using a doubly-linked-list ring instead of an array

--- day09.js
+++ day09.js
@@ -127,4 +127,4 @@ Amused by the speed of your answer, the Elves are curious:
 
 What would the new winning Elf's score be if the number of the last marble were 100 times larger?
 */
-console.log(marbleScore(455, 7122300);
+console.log(marbleScore(455, 7122300));
